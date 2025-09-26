7e3b2f9cf3240b7604042b037188996b55787ef8
leopiccionia <leopiccionia@gmail.com>
2025-09-26 16:17:28 -0300

    Adicionar taxonomia `funcao_musica` na single de agentes

--- Theme.php
+++ Theme.php
@@ -2,6 +2,7 @@
 namespace SOM;
 
 use MapasCulturais\App;
+use MapasCulturais\i;
 
 
 // class Theme extends \Subsite\Theme {
@@ -29,5 +30,21 @@ class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
         $app->hook('mapasculturais.body:after', function() use ($app) {
             $this->part('theme-css');
         });
+
+        $addTaxonomyToAgentEdit = function () {
+        ?>
+            <entity-terms :entity="entity" taxonomy="funcao_musica" editable classes="col-12" title="<?php i::_e('Função na música'); ?>"></entity-terms>
+        <?php
+        };
+        $app->hook('template(agent.edit.edit1-entity-info-taxonomie-area):before', $addTaxonomyToAgentEdit);
+        $app->hook('template(agent.edit.edit2-entity-info-taxonomie-area):before', $addTaxonomyToAgentEdit);
+
+        $addTaxonomyToAgentSingle = function () {
+        ?>
+            <entity-terms :entity="entity" taxonomy="funcao_musica" classes="col-12" hide-required title="<?php i::esc_attr_e('Função na música');?>"></entity-terms>
+        <?php
+        };
+        $app->hook('template(agent.single.single1-entity-info-taxonomie-area):before', $addTaxonomyToAgentSingle);
+        $app->hook('template(agent.single.single2-entity-info-taxonomie-area):before', $addTaxonomyToAgentSingle);
     }
 }
