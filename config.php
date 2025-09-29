4f079df7af44edc44e487d0f68b8c057e592a1b2
leopiccionia <leopiccionia@gmail.com>
2025-09-29 14:53:24 -0300

    Renomeia imagem

--- config.php
+++ config.php
@@ -4,5 +4,8 @@ return [
     'app.enabled.events' => false,
     'app.enabled.spaces' => false,
     'app.enabled.projects' => false,
+
     'app.siteName' => 'SOM',
+
+    'logo.image' => 'img/logo-som.png',
 ];
