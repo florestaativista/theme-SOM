7940fb6bea60da7f031b2a3a7eca4d0a6c2f180b
leopiccionia <leopiccionia@gmail.com>
2025-09-29 10:36:39 -0300

    Redireciona para edição de usuário em controllers autenticados

--- Theme.php
+++ Theme.php
@@ -17,7 +17,7 @@ class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
 
         $app = App::i();
 
-        $app->hook('auth.successful', function () use ($app) {
+        $app->hook('controller(<<*>>).requireAuthentication', function () use ($app) {
             if ($app->auth->isUserAuthenticated()) {
                 $profile = $app->user->profile;
                 if ($profile->validationErrors) {
