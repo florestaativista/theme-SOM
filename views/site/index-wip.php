da51120dc75324e68b4edd91f35e2fa0603035e3
leopiccionia <leopiccionia@gmail.com>
2025-09-29 17:44:15 -0300

    Página home (WIP)

--- views/site/index-wip.php
+++
@@ -1,8 +0,0 @@
-<div></div>
-
-<div class="som-home">
-    <div class="som-home__banner">
-        <img src="<?php $this->asset('img/som-banner.jpg') ?>" alt="">
-        <p class="som-home__hero">Viver de música poder ser mais do que correr atrás de plays em alguma Big Tech.</p>
-    </div>
-</div>
---
+++ views/site/index.php
@@ -0,0 +1,14 @@
+<div></div>
+
+<div class="som-home">
+    <div class="som-home__banner">
+        <img src="<?php $this->asset('img/som-banner.jpg') ?>" alt="SOM">
+        <div class="som-home__hero">
+            <p>Viver de música poder ser mais do que correr atrás de plays em alguma Big Tech.</p>
+            <div class="som-home__hero-button">
+                <a class="button button--primary button--large" href="#">Crie seu perfil agora</a>
+            </div>
+            <p>Uma plataforma desenvolvida para bandas e artistas independentes.</p>
+        </div>
+    </div>
+</div>
