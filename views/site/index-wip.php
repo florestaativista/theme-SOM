cb75d033f172462e8d67162a5e3b70944fc27fb8
leopiccionia <leopiccionia@gmail.com>
2025-09-29 16:42:32 -0300

    Página home (WIP)

---
+++ views/site/index-wip.php
@@ -0,0 +1,8 @@
+<div></div>
+
+<div class="som-home">
+    <div class="som-home__banner">
+        <img src="<?php $this->asset('img/som-banner.jpg') ?>" alt="">
+        <p class="som-home__hero">Viver de música poder ser mais do que correr atrás de plays em alguma Big Tech.</p>
+    </div>
+</div>
