a2b0b755c7089da9540674c4416bfee32fd726a9
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-09-29 22:23:26 -0300

    ajusta rotas de redirecionamento de perfil incompleto

--- Theme.php
+++ Theme.php
@@ -35,7 +35,7 @@ class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
             ";
         });
 
-        $app->hook('GET(<<*>>):before, -GET(agent.edit):before', function () use ($app) {
+        $app->hook('GET(<<*>>):before, -GET(agent.<<unlock|edit>>):before, -GET(auth.<<*>>):before', function () use ($app) {
             if ($app->auth->isUserAuthenticated()) {
                 $profile = $app->user->profile;
                 if ($profile->validationErrors) {
