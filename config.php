9aee4f76544f230714f09cc24f3d3c3aadeefd49
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-09-29 21:59:39 -0300

    remove configuração de offline do tema

--- config.php
+++ config.php
@@ -8,17 +8,4 @@ return [
     'app.siteName' => 'SOM',
 
     'logo.image' => 'img/logo-som.png',
-
-
-    'app.offline' => date('Y-m-d H:i:s') > '2025-09-19 09:00:00' && date('Y-m-d H:i:s') < '2025-10-03 08:00:00',
-    'app.offlineUrl' => '/em-breve/',
-    'app.offlineBypassFunction' => function() {
-        $senha = $_GET['online'] ?? '';
-        
-        if ($senha === env('OFFLINE_BYPASS')) {
-            $_SESSION['online'] = true;
-        }
-
-        return $_SESSION['online'] ?? false;
-    }
 ];
