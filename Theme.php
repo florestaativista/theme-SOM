e86617a077acb0d5938e065fda7269c06534b006
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-09-29 22:16:07 -0300

    filtro de agentes

--- Theme.php
+++ Theme.php
@@ -17,6 +17,24 @@ class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
 
         $app = App::i();
 
+        $app->hook('ApiQuery(Agent).joins', function(&$joins) use($app) {
+            $request = $app->request;
+
+            // Don't filter agents when listing users, editing profile, etc.
+            $listing_agents = $request->controllerId === 'agent' && $request->action === 'find';
+
+            if(!$listing_agents) {
+                return;
+            }
+
+            $joins .= "
+                JOIN e.user u
+                JOIN u.__metadata um WITH um.key = 'som_active' AND um.value = '1'
+                JOIN e.__termRelations tr
+                JOIN tr.term funcao WITH funcao.taxonomy = 'funcao_musica' AND funcao.term IS NOT NULL
+            ";
+        });
+
         $app->hook('GET(<<*>>):before, -GET(agent.edit):before', function () use ($app) {
             if ($app->auth->isUserAuthenticated()) {
                 $profile = $app->user->profile;
