b69ea021691dd379992c49d8725d35e942f823fc
leopiccionia <leopiccionia@gmail.com>
2025-10-02 17:51:37 -0300

    Ajusta textos da home

--- config.php
+++ config.php
@@ -12,4 +12,5 @@ return [
     'logo.image' => 'img/logo-som.png',
 
     'text:home-entities.opportunities' => i::__('Aqui você encontra as melhores oportunidades para apresentar sua música ao mundo, se conectar a diversos produtores, artistas, festivais, eventos e fazer parte do novo mapa da música. Clique e inscreva-se.'),
+    'text:home-opportunities.description' => i::__('Cadastre-se e participe de oportunidades'),
 ];
--- views/site/index.php
+++ views/site/index.php
@@ -12,11 +12,10 @@ $this->import("
     <div class="som-home__banner">
         <img src="<?php $this->asset('img/som-banner.jpg') ?>" alt="SOM">
         <div class="som-home__hero">
-            <p>Viver de música poder ser mais do que correr atrás de plays em alguma Big Tech.</p>
+            <p>Uma plataforma desenvolvida para bandas e artistas independentes.</p>
             <div class="som-home__hero-button">
                 <a class="button button--primary button--large" href="<?= $app->createUrl('panel', 'index') ?>">Crie seu perfil agora</a>
             </div>
-            <p>Uma plataforma desenvolvida para bandas e artistas independentes.</p>
         </div>
     </div>
 
