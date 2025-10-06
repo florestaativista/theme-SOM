edad9ef520994a9dfb0e61d61e2f326d8e9604e5
leopiccionia <leopiccionia@gmail.com>
2025-10-06 17:14:16 -0300

    Remove filtro por área da edição do espaço

---
+++ components/search-filter-space/template.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+use MapasCulturais\i;
+
+$this->import('
+    mc-multiselect
+    mc-tag-list
+    search-filter
+');
+?>
+<search-filter :position="position" :pseudo-query="pseudoQuery">
+    <label class="form__label">
+        <?= i::_e('Filtros de espaço') ?>
+    </label>
+    <form class="form" @submit="$event.preventDefault()">
+        <?php $this->applyTemplateHook('search-filter-space', 'begin') ?>
+        <div class="field search-filter__filter-space-status">
+            <label> <?php i::_e('Status do espaço') ?> </label>
+            <?php $this->applyTemplateHook('search-filter-space-field', 'before') ?>
+            <label> <input v-model="pseudoQuery['acessibilidade']" true-value="Sim" :false-value="undefined" type="checkbox"> <?php i::_e('Possui acessibilidade') ?> </label>
+            <label class="verified"> <input v-model="pseudoQuery['@verified']" type="checkbox"> <?php i::_e('Espaços oficiais') ?> </label>
+            <?php $this->applyTemplateHook('search-filter-space-field', 'after') ?>
+        </div>  
+        <div class="field search-filter__filter-space-types">
+            <label> <?php i::_e('Tipos de espaços') ?></label>
+
+            <mc-multiselect :model="pseudoQuery['type']" :items="types" placeholder="<?= i::esc_attr__('Selecione os tipos: ') ?>" hide-filter hide-button></mc-multiselect>
+            <mc-tag-list editable :tags="pseudoQuery['type']" :labels="types" classes="space__background space__color"></mc-tag-list>
+        </div>
+        <?php $this->applyTemplateHook('search-filter-space', 'end') ?>
+    </form>
+    <a class="clear-filter" @click="clearFilters()"><?php i::_e('Limpar todos os filtros') ?></a>
+</search-filter>
