4fd8be9e474092846fb64ccfdaf10d9fa1a31fd0
israelmelo <israelg.8@gmail.com>
2026-02-04 13:34:51 -0300

    Adiciona valor vazio para campo de país

--- conf-base.php
+++ conf-base.php
@@ -2,7 +2,8 @@
 use MapasCulturais\i;
 
 $_config = [
-    'footer.supportMessage' => sprintf(i::__("Precisa de ajuda? Acesse o chat no rodapé da página ou envie um email para %s para falar com nossa equipe de suporte."), '<a href="mail:contato@som.vc">contato@som.vc</a>')
+    'footer.supportMessage' => sprintf(i::__("Precisa de ajuda? Acesse o chat no rodapé da página ou envie um email para %s para falar com nossa equipe de suporte."), '<a href="mail:contato@som.vc">contato@som.vc</a>'),
+    'address.defaultCountryCode' => ''
 ];
 
 return $_config;
