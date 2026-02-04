205ef9346c85cddb15adaea5de6e4e9605d3fa4e
israelmelo <israelg.8@gmail.com>
2026-02-04 16:33:55 -0300

    Adiciona valor vazio para campo En_Pais

--- conf-base.php
+++ conf-base.php
@@ -3,7 +3,8 @@ use MapasCulturais\i;
 
 $_config = [
     'footer.supportMessage' => sprintf(i::__("Precisa de ajuda? Acesse o chat no rodapé da página ou envie um email para %s para falar com nossa equipe de suporte."), '<a href="mail:contato@som.vc">contato@som.vc</a>'),
-    'address.defaultCountryCode' => ''
+    'address.defaultCountryCode' => '',
+    'app.defaultCountry' => ''
 ];
 
 return $_config;
