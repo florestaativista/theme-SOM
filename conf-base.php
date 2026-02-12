d24d8ab57a556dcd91ee5e495768ac92d024c2d9
israelmelo <israelg.8@gmail.com>
2026-02-12 10:42:25 -0300

    Altera o email do mailer.from

--- conf-base.php
+++ conf-base.php
@@ -4,7 +4,8 @@ use MapasCulturais\i;
 $_config = [
     'footer.supportMessage' => sprintf(i::__("Precisa de ajuda? Acesse o chat no rodapé da página ou envie um email para %s para falar com nossa equipe de suporte."), '<a href="mail:contato@som.vc">contato@som.vc</a>'),
     'address.defaultCountryCode' => '',
-    'app.defaultCountry' => ''
+    'app.defaultCountry' => '',
+    'mailer.from' => 'plataformas@som.vc'
 ];
 
 return $_config;
